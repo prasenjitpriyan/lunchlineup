a6b45170fb713858b8ad40b420b6a8e6a13d02de
Prasenjit Das <prasenjit.webdev@icloud.com>
2025-02-05 21:54:41 +0530

    Mongodb added

---
+++ lib/mongodb.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const MONGODB_URI: string = process.env.MONGODB_URI as string
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable')
+}
+
+let cached = (global as any).mongoose || { conn: null, promise: null }
+
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn
+  }
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: 'Lunch_Lineup',
+        bufferCommands: false
+      })
+      .then((mongoose) => mongoose)
+  }
+  cached.conn = await cached.promise
+  return cached.conn
+}
